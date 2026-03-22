e7e5eaf775011f03c0d0df8b015d4195bcf7af1f
Atharva270606 <1012411168@despu.edu.in>
2026-03-22 13:10:29 +0530

    basic code for the directory tree

---
+++ tree_directory.java
@@ -0,0 +1,13 @@
+
+
+public class tree_directory 
+{
+    node root;
+    node current_pos; 
+    
+    public tree_directory()
+    {
+        root = new node("root");
+        current_pos = root;
+    }
+}
