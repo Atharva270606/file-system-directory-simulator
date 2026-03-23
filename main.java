4c69d049db3b06f17325805efd5bce31b24a30a1
1012411197-max <1012411197@despu.edu.in>
2026-03-23 10:33:50 +0530

    adding menu for the user to select function-Mrinank
    Implemented a file system directory simulator with options to create, delete, and search directories.

--- main.java
+++ main.java
@@ -1,1 +1,61 @@
+import java.util.Scanner;
+ public class main
+ {
+    public static void main(String[] args) 
+   {
 
+      Scanner sc = new Scanner(system.in);
+      tree_directory tree = new tree_directory();
+
+      int choice;
+
+      do{
+
+        System.out.println("\n********** file system directory simulator ***********");
+        System.out.println("1. Creating New Directory ");
+        System.out.println("2. Deleting a Directory");
+        System.out.println("3. Searching a Directory ");
+        System.out.println("4. Displaying a Tree ");
+        System.out.println("5. Exit ");\
+
+        System.out.println("Enter Your Choice: ");
+        choice = sc.nextInt();
+        sc.nextLine();
+
+        switch (choice) {
+
+          case 1: 
+            System.out.print("Enter your directory name:");
+            String name = sc.nextLine();
+            tree.mkdir(name);
+            break;
+
+          case 2: 
+            System.out.println("Enter directory name to delete: ");
+            String dele = sc.nextLine();
+            tree.delete(dele);
+            break;
+
+          case 3:
+            System.out.println("Enter directory name to search : ");
+            string search = sc.nextLine();
+
+            node result =  tree.search(tree.root, search);
+
+            if (result != null)
+            {
+              System.out.println("The Directory is found ");
+            }
+            else
+            {
+              System.out.println("The Directory is not found ");
+            }
+              break;
+
+        }while(choice != 5)
+
+          sc.close();
+      }
+   }
+            
+            
