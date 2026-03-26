6284ae1cb756afe8c886530db8c743a34cd25801
1012411197-max <1012411197@despu.edu.in>
2026-03-26 23:27:00 +0530

    Refactor directory simulator input and output

--- main.java
+++ main.java
@@ -5,57 +5,87 @@ import java.util.Scanner;
    {
 
       Scanner sc = new Scanner(System.in);
-      tree_directory tree = new tree_directory();
+
+      System.out.print("Enter maximum number of directories");
+      int size = sc.nextInt();
+      sc.nextLine();
+
+      tree_directory tree = new tree_directory(size);
 
       int choice;
 
-      do{
+      do(
 
-        System.out.println("\n********** file system directory simulator ***********");
-        System.out.println("1. Creating New Directory ");
-        System.out.println("2. Deleting a Directory");
-        System.out.println("3. Searching a Directory ");
-        System.out.println("4. Displaying a Tree ");
-        System.out.println("5. Exit ");
+           System.out.println("\n***** File System directory simulator*****");
+           System.out.println("1. Creating a Directory");
+           System.out.println("2. Delete a Directory");
+           System.out.println("3. Search a Directory");
+           System.out.println("4. Display the Tree");
+           System.out.println("5. Exit a Menu ");
 
-        System.out.println("Enter Your Choice: ");
-        choice = sc.nextInt();
-        sc.nextLine();
 
-        switch(choice) 
-        {
+           System.out.println("Enter your choice");
+           choice = sc.nextInt();
+           sc.nextLine();
 
-          case 1: 
-            System.out.print("Enter your directory name:");
-            String name = sc.nextLine();
-            tree.mkdir(name);
-            break;
-
-          case 2: 
-            System.out.println("Enter directory name to delete: ");
-            String dele = sc.nextLine();
-            tree.delete(dele);
-            break;
-
-          case 3:
-            System.out.println("Enter directory name to search : ");
-            String search = sc.nextLine();
-
-            node result =  tree.search(tree.root, search);
-
-            if (result != null)
-            {
-              System.out.println("The Directory is found ");
-            }
-            else
-            {
-              System.out.println("The Directory is not found ");
-            }
-              break;
-          }  
-        }while(choice != 4);
-
-          sc.close();
-    }
-  }          
+           switch (choice)
             
+           {
+
+
+            case 1: 
+                System.out.print("1. Enter your directory name: ");
+                string name = sc.nextLine();
+                tree.mkdir(name);
+                break;
+
+            case 2: 
+                System.out.print("1. Creating the  directory name you want to delete ");
+                string dele = sc.nextLine();
+                tree.delete(dele);
+                break;
+
+            case 3 :
+                System.out.print("Enter the directory name you want to search: ");
+                String search = sc.nextLine();
+
+                node result = tree.search(tree.root, search );
+
+                if (result != null) {
+                    System.out.println("The directory is Found!  ");
+                } else {
+                    System.out.println("1. The directory is not found");
+                }
+                break;
+
+           }
+     
+   } while (choice !=5);
+
+    sc.close();
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
